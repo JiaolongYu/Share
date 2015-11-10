e611db4f6915a8e0b2ee3f4bde967a34a8441a8a
joce <julorwowo@gmail.com>
2015-11-09 19:53:21 -0600

    my tests

--- compute_tests.py
+++ compute_tests.py
@@ -1,16 +1,54 @@
-__author__ = 'Jiaolong Yu'
-
+from unittest import TestCase
 import randomized_input
 import compute_highest_affinity
 
-num_lines = 10000
-num_users = 1000
 
-site_list = randomized_input.randomized_site_list(num_lines)
-user_list = randomized_input.randomized_user_list(num_lines, num_users)
-time_list = xrange(0,num_lines)
+class StandAloneTests(TestCase):
+	def test_compute1(self):
+		num_lines = 10000
+		num_users = 1000
 
-computed_result = compute_highest_affinity.highest_affinity(site_list, user_list, time_list)
-expected_result = ("facebook", "google")
-print computed_result
-assert computed_result == expected_result
+		site_list = randomized_input.randomized_site_list(num_lines)
+		user_list = randomized_input.randomized_user_list(num_lines, num_users)
+		time_list = xrange(0,num_lines)
+
+		computed_result = compute_highest_affinity.highest_affinity(site_list, user_list, time_list)
+		expected_result = ("facebook", "google")
+		if computed_result == expected_result:
+			self.assertTrue(True)
+		else:
+			self.assertTrue(False) 
+
+	# def test_compute2(self):
+	# 	a = "a"
+	# 	b = "b"
+	# 	c = "c"
+	# 	d = "d"
+	# 	e = "e"
+	# 	f = "f"
+	# 	g = "g"
+	# 	h = "h"
+
+	# 	A = "A"
+	# 	B = "B"
+	# 	C = "C"
+	# 	D = "D"
+	# 	E = "E"
+	# 	F = "F"
+	# 	G = "G"
+	# 	H = "H"
+
+
+	# 	site_list = [a,a,b,c,c,c,d,d,e,e,f,f,g]
+	# 	user_list = [B,D,C,A,B,C,B,C,C,D,A,B,A]
+
+	# 	time_list = range(0,13)
+
+	# 	computed_result = compute_highest_affinity.highest_affinity(site_list, user_list, time_list)
+	# 	expected_result1 = (c, d)
+	# 	expected_result2 = (c, f)
+
+	# 	if computed_result == expected_result1 or computed_result == expected_result2:
+	# 		self.assertTrue(True)
+	# 	else:
+	# 		self.assertTrue(False)
